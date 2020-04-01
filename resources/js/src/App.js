5f93af6e5aa239b7bdec4414606d33e5125f71df
Douglas Harrington Muhone <theeomm@outlook.com>
2020-04-01 09:16:14 +0200

    initial commit

---
+++ resources/js/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">App Component</div>
+
+                        <div className="card-body">I'm an App component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
+
+
